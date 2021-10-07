f2a2cfdb5262229acef15dc6d176a7eb12acfabf
Mustapha <hamadi.mustapha7@gmail.com>
2021-10-07 19:44:13 +0200

    add text changer

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ let lower = document.querySelector(".lower");
 let lower2 = document.querySelector(".lower__card-2");
 let main = document.querySelectorAll(".main__social");
 let card = document.querySelectorAll(".lower__card");
+
 checkbox.addEventListener("change", () => {
+  nameToggle();
   header.classList.toggle("light-mode");
   lower.classList.toggle("light-mode");
   lower2.classList.toggle("light-mode");
@@ -16,3 +18,12 @@ checkbox.addEventListener("change", () => {
     e.classList.toggle("light-mode-card");
   });
 });
+
+const nameToggle = () => {
+  let x = document.querySelector(".upper__mode");
+  if (x.innerText === "Light mode") {
+    x.innerText = "Dark mode";
+  } else {
+    x.innerText = "Light mode";
+  }
+};
